371c164e972a0712e5a79c5c992cc05cf2005ea7
Rishabh Bhatia <rishabh.bhatia08@gmail.com>
2016-09-15 21:24:21 +0530

    Restrict posting an empty string comment

--- www/js/app.js
+++ www/js/app.js
@@ -459,7 +459,7 @@ app.directive('hideTabBar', function($timeout) {
   $scope.postComment =function() {
     console.log("Post user comment: "+$scope.usercomment);
 
-    if($scope.usercomment === undefined) return;
+    if($scope.usercomment === undefined || $scope.usercomment === '') return;
 
     var usercommentobject = {
     "commentby" : {
